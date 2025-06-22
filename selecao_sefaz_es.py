9485539960bf7806c83760a6f6a5a41c8a573c4f
João Oliveira <joao.oliveira@directy.com.br>
2025-06-22 14:44:03 -0300

    Feat: Estabelecendo conexão com o banco(ou criando)

---
+++ selecao_sefaz_es.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# 1) Para conectar (ou criar) o arquivo de banco
+conn = sqlite3.connect("selecao_sefaz_es.db")
+cursor = conn.cursor()
+
+print("Conexão estabelecida com sucesso, SQLite!")
+
+# 2) Criar as tabelas SELECAO_CANDIDATO e SELECAO_TESTE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS SELECAO_CANDIDATO (
+    ID_CANDIDATO INTEGER PRIMARY KEY AUTOINCREMENT,
+    NME_CANDIDATO TEXT NOT NULL,
+    DAT_INSCRICAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS SELECAO_TESTE (
+    ID_TESTE INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_CANDIDATO INTEGER NOT NULL,
+    NUM_FIBONACCI INTEGER NOT NULL,
+    NUM_PAR INTEGER NOT NULL CHECK (NUM_PAR IN (0,1)),
+    NUM_IMPAR INTEGER NOT NULL CHECK (NUM_IMPAR IN (0,1)),
+    FOREIGN KEY (ID_CANDIDATO) REFERENCES SELECAO_CANDIDATO(ID_CANDIDATO)
+);
+""")
+
+# Salva as alterações no banco
+conn.commit()
+print(" Tabelas criadas com sucesso!")
